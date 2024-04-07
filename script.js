2dd5a82f6edfa5422e7cf229b8929bc4fcd5eaba
Gift Jacksun <giftjacksun@gmail.com>
2024-04-07 07:43:26 +0100

    I added the Enter key, made it functional, fixed the bugs and styled the components.

--- script.js
+++ script.js
@@ -1,52 +1,67 @@
 const display = document.getElementById("display");
-//backspace functionality
+
+// backspace functionality
 function backspace() {
   let text = String(display.value);
   display.value = text.slice(0, -1);
+  display.focus();
 }
-//display btns
+
+// display btns
 let btns = document.querySelectorAll("button");
 btns.forEach((element) => {
   if (
-    element.textContent !== "backspace" &&
     element.textContent !== "CapsLock" &&
-    element.textContent !== "Spacebar"
+    element.textContent !== "Spacebar" &&
+    element.textContent !== "Enter ↓"
   ) {
-    element.addEventListener("click", () => {
-      display.value += element.textContent;
-    });
+    if (element.id !== "backspace" && element.id !== "enter") {
+      element.addEventListener("click", () => {
+        display.value += element.textContent;
+        display.focus();
+      });
+    }
   }
 });
-//capslock functionality
+
+// capslock functionality
 const CapsLock = document.getElementById("CapsLock");
 CapsLock.addEventListener("click", () => {
   if (CapsLock.textContent === "CapsLock") {
     btns.forEach((element) => {
       if (
-        element.textContent !== "backspace" &&
         element.textContent !== "CapsLock" &&
-        element.textContent !== "Spacebar"
+        element.textContent !== "Spacebar" &&
+        element.textContent !== "Enter ↓"
       ) {
         element.textContent = element.textContent.toUpperCase();
+        display.focus();
       }
     });
     CapsLock.textContent = "CapsLock on";
   } else if (CapsLock.textContent === "CapsLock on") {
     btns.forEach((element) => {
       if (
-        element.textContent !== "backspace" &&
         element.textContent !== "CapsLock" &&
-        element.textContent !== "Spacebar"
+        element.textContent !== "Spacebar" &&
+        element.textContent !== "Enter ↓"
       ) {
         element.textContent = element.textContent.toLowerCase();
+        display.focus();
       }
     });
     CapsLock.textContent = "CapsLock";
   }
 });
-//Spacebar functionality
+
+// Spacebar functionality
 function spacebar() {
   display.value += " ";
+  display.focus();
 }
 
-
+// Enter key functionality
+const enter = () => {
+  display.value += "\n";
+  display.focus();
+};
